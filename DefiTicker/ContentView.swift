52265521a9cc75b899dbb6ba140c05c71f93d3fc
Felix Chon <seoungyu@ualberta.ca>
2020-11-18 23:48:22 +0900

    Initial Commit

---
+++ DefiTicker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DefiTicker
+//
+//  Created by Chon, Felix | Felix | DCMS on 2020/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
